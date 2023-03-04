ee4f8fe90d161ef47483f09a2adfa6f4cb00a526
HirokiYamamoto <hiroki200328@icloud.com>
2023-03-04 19:34:41 +0900

    コメント追加 くそ早くしない限りは平気だったからヨシ

--- Assets/Scripts/RightMoveBlock.cs
+++ Assets/Scripts/RightMoveBlock.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class RightMoveBlock : MonoBehaviour
 {
-	public bool isMove = false;
-	public bool isKabe = false;
-
+	[Header("移動速度")]
 	public float moveSpeed;
+
+	[Header("動く状態かどうかのフラグ")]
+	public bool isMove = false;
+	[Header("壁との衝突判定")]
+	public bool isFloor = false;
 
 	Rigidbody2D rb;
 	// Start is called before the first frame update
@@ -19,25 +22,21 @@ public class RightMoveBlock : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		if (isMove && !isKabe)
+		//円の中かつ衝突判定がない場合に
+		if (isMove && !isFloor)
 		{
+			//動かす
 			transform.position += new Vector3(moveSpeed, 0,0);
 		}
 	}
 
 	private void OnCollisionEnter2D(Collision2D collision)
 	{
+		//動いてる状態かつ当たったオブジェクトが床の時に
 		if (collision.gameObject.tag == "Floor")
 		{
-			if(isMove)isKabe = true;
-		}
-	}
-
-	private void OnCollisionExit2D(Collision2D collision)
-	{
-		if (collision.gameObject.tag == "Floor")
-		{
-			isKabe = false;
+			// 衝突判定をTrueに
+			if(isMove)isFloor = true;
 		}
 	}
 }
