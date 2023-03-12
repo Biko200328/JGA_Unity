a8a1ab3a9136ca8610c62c61b8c4fb792ca1e933
HirokiYamamoto <hiroki200328@icloud.com>
2023-03-12 17:31:10 +0900

    fade導入 Rキーでシーンリセット追加 ランプ持ってる時に一方通行床とのバグを解消

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+	// フェード用のシーンコントローラー
+	SceneController sceneController;
+
 	// Start is called before the first frame update
 	void Start()
 	{
@@ -13,11 +17,18 @@ public class GameManager : MonoBehaviour
 
 		// fpsの固定
 		Application.targetFrameRate = 60;
+
+		GameObject camera = GameObject.Find("Main Camera");
+		sceneController = camera.GetComponent<SceneController>();
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-
+		if(Input.GetKeyDown(KeyCode.R))
+		{
+			// 現在のシーンを読み直す
+			sceneController.sceneChange(SceneManager.GetActiveScene().name);
+		}
 	}
 }
