0920b977758e82a1501a7558ba1fd5a239843fb5
HirokiYamamoto <hiroki200328@icloud.com>
2023-03-13 11:58:32 +0900

    消しちゃいけないものも消してしまっていたので修正

--- Assets/Scripts/PlayerCircle.cs
+++ Assets/Scripts/PlayerCircle.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class PlayerCircle : MonoBehaviour
 {
+	[Header("円の最大直径")]
+	public float maxCircleSize = 1;
+
+	[Header("円の最小直径")]
+	public float minCircleSize = 0;
+
+	[Header("サイズ増減数値")]
+	[SerializeField] float changeSize;
+
+	// 現在の円の範囲
+	private float circleSize;
+
 	PlayerMove playerMove;
 
 
@@ -17,7 +29,38 @@ public class PlayerCircle : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		
+		if (playerMove.isLightOn)
+		{
+			//最大値より低かったら足す
+			if (circleSize < maxCircleSize)
+			{
+				circleSize += changeSize;
+			}
+
+			//最大値より大きかったら最大値に合わせる
+			if (circleSize > maxCircleSize)
+			{
+				circleSize = maxCircleSize;
+			}
+		}
+		else
+		{
+			// 最小値(0)より大きかったら引く
+			if (circleSize > minCircleSize)
+			{
+				circleSize -= changeSize;
+			}
+
+			//最小値より小さかったら最小値に合わせる
+			if (circleSize < minCircleSize)
+			{
+				circleSize = minCircleSize;
+			}
+		}
+
+		var circleVec = new Vector3(circleSize, circleSize, circleSize);
+
+		transform.localScale = circleVec;
 	}
 
 	private void OnTriggerEnter2D(Collider2D collision)
@@ -32,7 +75,7 @@ public class PlayerCircle : MonoBehaviour
 		}
 
 		// 蛇ブロック
-		if(collision.gameObject.tag == "growOriginal")
+		if (collision.gameObject.tag == "growOriginal")
 		{
 			Growth growth = collision.GetComponent<Growth>();
 			growth.isLightIn = true;
