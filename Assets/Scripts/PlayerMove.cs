1d15208bdfcd3857b82a33556a3a259eadd72590
HirokiYamamoto <hiroki200328@icloud.com>
2023-03-27 17:49:39 +0900

    ママのシーンを少し変更

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,7 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.PackageManager.UI;
-using UnityEditor.U2D.IK;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
